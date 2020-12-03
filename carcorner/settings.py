73d8a365cd6905c60f06ffbf51101bb85cd31f7b
subashlimbu <prajan_l@hotmail.com>
2020-12-03 11:00:02 +0000

    Completed Teams Model

--- carcorner/settings.py
+++ carcorner/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'carcorner.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carcorner_db',
+        'USER': 'postgres',
+        'PASSWORD': 'qwert123',
+        'HOST': 'localhost'
     }
 }
 
@@ -123,3 +126,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'carcorner/static'),
 ]
+
+# Media settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
