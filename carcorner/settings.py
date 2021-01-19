b55baa492fea42a410cbf91273742e6ab385b113
subashlimbu <prajan_l@hotmail.com>
2021-01-19 22:06:35 +0000

    Send email functionality implemented

--- carcorner/settings.py
+++ carcorner/settings.py
@@ -153,3 +153,10 @@ MESSAGE_TAGS = {
 }
 
 SITE_ID = 1
+
+#Email sending
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_HOST_USER = 'subashlimbu700@gmail.com'
+EMAIL_HOST_PASSWORD = 'rbnuoxpmdvtsxaxa'
+EMAIL_USE_TLS = True
